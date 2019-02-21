5c724bb79cbefb7c38f287c3d1ae2aaf001834bb
vanceleon <vanceleon44@gmail.com>
2019-02-21 12:30:10 -0500

    building nav hamburger menu created projects file

--- portfolio/src/Components/NavBar.js
+++ portfolio/src/Components/NavBar.js
@@ -1,1 +1,35 @@
-imort
+import React, { Component } from 'react';
+
+class NavMenu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false,
+      event: false
+    };
+  }
+
+  onClick = () => {
+      this.setState({isOpen: !this.state.isOpen});
+      if(this.state.event){
+          this.setState({event: false});
+          window.removeEventListener("click",this.handleEvent);
+      }else{
+          this.setState({event: true});
+          window.addEventListener('click', this.handleEvent);
+      }
+  };
+
+
+  handleEvent(ev) {
+      ev.preventDefault();
+      
+  }
+
+  render() {
+    const burgerStyle = ['hamburger', 'hamburger--spin'];
+    return <div>menu</div>;
+  }
+}
+
+export default NavMenu;
