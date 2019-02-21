97f4690cf836d46a22459eec11a0bb39a0752d6a
vanceleon <vanceleon44@gmail.com>
2019-02-21 11:55:43 -0500

    changing tab logo building header component

---
+++ portfolio/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <div className="title">
+            <div className="header">
+                <h1>Hi, I'm</h1> 
+                <h1>Vance</h1> 
+                <h1>Leon</h1> 
+            </div>
+        </div>
+    )
+}
+export default Header;
