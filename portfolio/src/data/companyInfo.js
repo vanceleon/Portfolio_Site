539614958987bd49c74ca7a3d973a6f468477c6f
vanceleon <vanceleon44@gmail.com>
2019-02-22 16:26:54 -0500

    adding logo images

--- portfolio/src/data/companyInfo.js
+++ portfolio/src/data/companyInfo.js
@@ -5,7 +5,31 @@ export const companyInfo = [
     companyUrl: 'https://lambdaschool.com/',
     title: 'Full Stack Developer',
     location: 'New York, NY',
-    description: 'this is where you will write about companies'
+    description: 'Lambda School is a full immersive full stack software development program'
+  },
+  {
+    image: 'image src goes here',
+    company: 'California State University Fullerton',
+    companyUrl: 'https://lambdaschool.com/',
+    title: 'Equity Analyst (Buy-Side)',
+    location: 'Fullerton, CA',
+    description: `ASAP was an investment program for top tiered finance students to manage the university's endowment fund.` 
+  },
+  {
+    image: 'image src goes here',
+    company: 'Waddell & Reed',
+    companyUrl: 'https://lambdaschool.com/',
+    title: 'Financial Planning and Investments Intern',
+    location: 'Brea, CA',
+    description: 'evaluated investment opportunities and presented stock information to fellow advisors.'
+  },
+  {
+    image: 'image src goes here',
+    company: 'Injoy Life Resources',
+    companyUrl: 'https://lambdaschool.com/',
+    title: 'Life Coach',
+    location: 'New York, NY',
+    description: 'Helped a man with a mental disability develop life skills and move towards proper behavior in a social setting.'
   },
   {
     image: 'image src goes here',
@@ -17,34 +41,26 @@ export const companyInfo = [
   },
   {
     image: 'image src goes here',
-    company: 'Lambda School',
+    company: 'City of Garden Grove',
     companyUrl: 'https://lambdaschool.com/',
-    title: 'Full Stack Developer',
-    location: 'New York, NY',
-    description: 'this is where you will write about companies'
+    title: 'IT Support Technician',
+    location: 'Garden Grove, CA',
+    description: 'Supported city employees with IT needs.'
   },
   {
     image: 'image src goes here',
-    company: 'Lambda School',
+    company: 'RC Smith Design Build',
     companyUrl: 'https://lambdaschool.com/',
-    title: 'Full Stack Developer',
-    location: 'New York, NY',
-    description: 'this is where you will write about companies'
+    title: 'Bookkeeper',
+    location: 'Garden Grove, CA',
+    description: 'Supported city employees with IT needs.'
   },
   {
     image: 'image src goes here',
-    company: 'Lambda School',
+    company: 'Anaheim Automation',
     companyUrl: 'https://lambdaschool.com/',
-    title: 'Full Stack Developer',
-    location: 'New York, NY',
-    description: 'this is where you will write about companies'
-  },
-  {
-    image: 'image src goes here',
-    company: 'Lambda School',
-    companyUrl: 'https://lambdaschool.com/',
-    title: 'Full Stack Developer',
-    location: 'New York, NY',
-    description: 'this is where you will write about companies'
+    title: 'Web Designer',
+    location: 'Anaheim, CA',
+    description: 'A part of a web team that revamped the entire site and its content.'
   }
 ];
