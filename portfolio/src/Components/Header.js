cacce1306c972934b41ad9c06215ae8d97248772
vanceleon <vanceleon44@gmail.com>
2019-02-25 13:52:40 -0500

    attempts to parallax

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import headshot from '../img/LowerManhattan.jpg';
+// import headshot from '../img/LowerManhattan.jpg';
 
 const Header = () => {
     return (
@@ -7,7 +7,8 @@ const Header = () => {
             <div className="header">
                 <h1>Hi, I'm</h1> 
                 <h1>Vance Leon</h1> 
-                <img src={headshot} alt='headhshot' className='headShot'/> 
+                {/* <img src={headshot} alt='headhshot' className='parallal-headShot'/>  */}
+                <div className="parallax-headshot"></div>
             </div>
         </div>
     )
