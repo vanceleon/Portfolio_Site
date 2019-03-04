90c3bbf17301ea493e9b689f2a87d1263c13d5b6
vanceleon <vanceleon44@gmail.com>
2019-03-04 14:02:50 -0500

    new images and styling

--- portfolio/src/Components/Footer.js
+++ portfolio/src/Components/Footer.js
@@ -5,24 +5,40 @@ import Resume from '../pdf/VanceLeon.pdf';
 import resumePNG from '../img/social/Resume.png';
 
 const Footer = () => {
-    return (
-        <div className="footer" id='contact'>
-            <div className="footer-quote">
-                <h1>Californian to New Yorker</h1>
-                <h1>Let's Find Great Food</h1>
-                <h1>And Build A Project Along The Way</h1>
-                <a href="mailto:vanceleon44@gmail.com">Send me an email</a> 
-            </div>
-            <div className="contact-info">
-                <a href={Resume}><img className='social-media' src={resumePNG} alt='githubLogo'/></a>
-                <a href='https://github.com/vanceleon'><img className='social-media' src={github} alt='githubLogo'/></a>
+  return (
+    <div className='footer' id='contact'>
+      <div className='footer-quote'>
+        <h1>Californian to New Yorker</h1>
+        <h1>Let's Connect, Find Great Food</h1>
+        <h1>And Build A Project Along The Way</h1>
+      </div>
+      <div className='contact-info'>
+        <a className='social-media' href='mailto:vanceleon44@gmail.com'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+          >
+            <path d='M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z' />
+            <path d='M0 0h24v24H0z' fill='none' />
+          </svg>
+        </a>
 
-                <a href='https://www.linkedin.com/in/vanceleon/'><img className='social-media' src={linkedIn} alt='resume'/></a>
+        <a href={Resume}>
+          <img className='social-media' src={resumePNG} alt='githubLogo' />
+        </a>
+        <a href='https://github.com/vanceleon'>
+          <img className='social-media' src={github} alt='githubLogo' />
+        </a>
 
-            </div>
-            {/* <div className="email"> */}
-            {/* </div> */}
-        </div>
-    );
-} 
+        <a href='https://www.linkedin.com/in/vanceleon/'>
+          <img className='social-media' src={linkedIn} alt='resume' />
+        </a>
+      </div>
+      {/* <div className="email"> */}
+      {/* </div> */}
+    </div>
+  );
+};
 export default Footer;
