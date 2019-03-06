680da973c2b5bbf4add70dee17e5d2c6946a1cb8
vanceleon <vanceleon44@gmail.com>
2019-03-06 13:24:11 -0500

    took out redundant divs and trying different stlying

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -11,24 +11,16 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        {/* <div className="App-Container"> */}
 
-        <div className='header-view'>
-          <NavMenu />
-          <HeaderParallax />
-        </div>
+        {/* <div className='header-view'> */}
+        <NavMenu />
+        <HeaderParallax />
+        {/* </div> */}
         <div className='portfolio-container'>
-          {/* <Quotes /> */}
           <Projects />
           <Experience />
-
         </div>
-
         <Footer />
-        {/* <div className='testing'>
-          testing the text under image
-        </div> */}
-        {/* </div> */}
       </div>
     );
   }
